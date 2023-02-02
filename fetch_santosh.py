d4cddedd3d5daa57bed10be9c174ba0ec866b0eb
Santosh <ch.santoshkumar211@gmail.com>
2023-02-02 10:15:37 -0600

    updating code

--- fetch_santosh.py
+++ fetch_santosh.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from collections import deque, defaultdict
 import csv
 from sys import argv
+import json
 
 
 class read_data:
@@ -37,7 +38,7 @@ class transaction:
         self.amount = int(amount)
 
     def load_data(self, file_name="transactions.csv") -> None:
-        """loads the transaction data and populats required data structures with values
+        """loads the transaction data and populates required data structures with values
 
         Args:
             file_name (str, optional): file_name that contains the transactions. Defaults to "transactions.csv".
@@ -50,14 +51,21 @@ class transaction:
             if points < 0:
                 # Negative points indicate debit, so follow the order of transactions to debit the amount
                 while points < 0:
-                    # Continue debiting the amount untill points become zero
-                    first_payer = self.order.popleft()
-                    first_points = self.payers[first_payer].popleft()
-                    points, first_points = points + first_points, first_points + points
-                    if first_points > 0:
-                        # If the last debit has some points left add them back to the order
-                        self.order.appendleft(first_payer)
-                        self.payers[first_payer].appendleft(first_points)
+                    try:
+                        # Continue debiting the amount untill points become zero
+                        first_payer = self.order.popleft()
+                        first_points = self.payers[first_payer].popleft()
+                        points, first_points = (
+                            points + first_points,
+                            first_points + points,
+                        )
+                        if first_points > 0:
+                            # If the last debit has some points left add them back to the order
+                            self.order.appendleft(first_payer)
+                            self.payers[first_payer].appendleft(first_points)
+                    except:
+                        print("Invalid transactions!")
+                        exit(0)
             else:
                 self.order.append(payer)
                 self.payers[payer].append(points)
@@ -69,16 +77,22 @@ class transaction:
             dict: Dictonary containing payer names as keys and this remaining balance as values
         """
         amount = self.amount
-        while amount > 0:
-            payer = self.order.popleft()
-            if self.payers[payer]:
-                transaction_points = self.payers[payer].popleft()
-                if transaction_points > amount:
-                    self.payers[payer].append(transaction_points - amount)
-                    amount = 0
-                    break
-                else:
-                    amount -= transaction_points
+        total_funds = 0
+        for payer in self.payers:
+            total_funds += sum(self.payers[payer])
+        if total_funds < amount:
+            print("Insufficient points!")
+        else:
+            while amount > 0:
+                payer = self.order.popleft()
+                if self.payers[payer]:
+                    transaction_points = self.payers[payer].popleft()
+                    if transaction_points > amount:
+                        self.payers[payer].append(transaction_points - amount)
+                        amount = 0
+                        break
+                    else:
+                        amount -= transaction_points
 
         ret = {}
         for payer in self.payers:
@@ -89,13 +103,19 @@ class transaction:
 
 
 def main():
-    if len(argv) < 2:
-        print("Please use the format: python3 fetch_santosh.py $AMOUNT")
-        return
-    amount = argv[1]
-    transaction_obj = transaction(amount)
-    transaction_obj.load_data()
-    print(transaction_obj.solve())
+    try:
+        if len(argv) < 2:
+            print("Please use the format: python3 fetch_santosh.py [AMOUNT]")
+            return
+        amount = int(argv[1])
+        if amount < 0:
+            print("Negative amount is not allowed")
+        else:
+            transaction_obj = transaction(amount)
+            transaction_obj.load_data()
+            print(json.dumps(transaction_obj.solve(), indent=4))
+    except:
+        print("An error occurred during execution")
 
 
 if __name__ == "__main__":
